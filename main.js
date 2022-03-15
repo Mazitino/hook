b5f25e3b4072b8a688fda299d851942c51a9f17e
aydar.mazitov <aydar.mazitov@gmail.com>
2022-03-15 14:35:04 +0500

    Подправил скрипт конекта mogoose.

--- main.js
+++ main.js
@@ -13,12 +13,11 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Db
+//DataBase
 const db = require("./models");
 
 db.mongoose
     .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
-        useNewUrlPasrser: true,
         useUnifiedTopology: true
     })
     .then(() => {
@@ -29,3 +28,24 @@ db.mongoose
         console.error("Connection error", err);
         process.exit();
     });
+
+ // Smple route
+ app.get("/", (req, res) => {
+    res.json({ message: "Welcome to application." });
+  });
+
+
+  // Set port, listen for requests
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+  });
+
+  //Вывод всех пользователей (для проверки)
+function check_users() {
+    const User = db.user;
+    User.find({}, function(err, users){
+      if(err) return console.log(err);
+      console.log(users);
+    }); 
+}
